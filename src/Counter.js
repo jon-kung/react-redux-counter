34ee4fad024dbb311ed75c4c7c32b7b1e0d015c6
Jonathan <jkung92@gmail.com>
2019-05-29 16:04:39 -0700

    Added mapDispatchToProps

--- src/Counter.js
+++ src/Counter.js
@@ -2,26 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
   class Counter extends Component {
-    increment = () => {
-      this.props.dispatch({type: 'INCREMENT'})
-    }
-    decrement = () => {
-      this.props.dispatch({type: 'DECREMENT'})
-    }
-
     render() {
       return(
         <div>
           <h2> Counter </h2>
           <div>
-            <button onClick={this.decrement}> - </button>
+            <button onClick={this.props.decrement}> - </button>
             <span>{this.props.count}</span>
-            <button onClick={this.increment}> + </button>
+            <button onClick={this.props.increment}> + </button>
           </div>
         </div>
       );
     }
   }
+
+  // Can move this to "Container" to keep Counter clean
  
   function mapStateToProps(state){
     return {
@@ -29,4 +24,11 @@ import { connect } from 'react-redux';
     }
   }
 
-  export default connect(mapStateToProps)(Counter);
+  function mapDispatchToProps(dispatch){
+    return {
+      increment : () => dispatch({type: 'INCREMENT'}),
+      decrement : () => dispatch({type: 'DECREMENT'})
+    } 
+  }
+
+  export default connect(mapStateToProps, mapDispatchToProps)(Counter);
